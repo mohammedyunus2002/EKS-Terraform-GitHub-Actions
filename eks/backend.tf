6ed14d38f2fb3f06a948c6d14ae1b2b07ee8bc19
mohammedyunus2002 <89659794+mohammedyunus2002@users.noreply.github.com>
2024-11-25 12:01:39 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
+    bucket         = "myawsbucket-11222"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
